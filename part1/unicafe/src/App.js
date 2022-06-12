f99f54042343c06f02654bf305c2beaaadc8a7d4
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2022-06-12 12:13:36 +0530

    Refactor Statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,17 +1,27 @@
 import { useState } from 'react'
-
+const Button = ({buttonText, eventHandler}) => {
+  return (
+      <button onClick={eventHandler}>{buttonText}</button>
+  )
+}
+const StatisticLine = ({statName, data}) => {
+  return (<tr><td> {statName} {data} </td></tr>)
+}
 const Feedback = ({setGood, setNeutral, setBad}) => {
   return (
       <div>
         <h1>give feedback</h1>
-        <button onClick={setGood}> good </button>
-        <button onClick={setNeutral}> neutral </button>
-        <button onClick={setBad}> bad </button>
+        <Button buttonText={"good"} eventHandler={setGood} />
+        <Button buttonText={"neutral"} eventHandler={setNeutral} />
+        <Button buttonText={"bad"} eventHandler={setBad} />
       </div>
   )
 }
 
 const Statistics = ({good, neutral, bad}) => {
+  const total = good + bad + neutral
+  const average = ((good - bad)/total).toFixed(1)
+  const positive = (good/total * 100).toFixed(1) + "%"
   return (
       <div>
         <h1> Statistics </h1>
@@ -19,34 +29,12 @@ const Statistics = ({good, neutral, bad}) => {
           {good + bad + neutral > 0 ?
               <table>
                 <tbody>
-                <tr>
-                  <td> good </td>
-                  <td>{good}</td>
-                </tr>
-
-                <tr>
-                  <td> neutral </td>
-                  <td>{neutral}</td>
-                </tr>
-
-                <tr>
-                  <td> bad </td>
-                  <td>{bad}</td>
-                </tr>
-
-                <tr>
-                  <td> all </td>
-                  <td>{good + neutral + bad}</td>
-                </tr>
-
-                <tr>
-                  <td> average </td>
-                  <td>{((good - bad)/(good + neutral + bad)).toFixed(1)}</td>
-                </tr>
-                <tr>
-                  <td> positive </td>
-                  <td>{(good/(good + neutral + bad) * 100).toFixed(1)}%</td>
-                </tr>
+                  <StatisticLine statName={"good"} data={good} />
+                  <StatisticLine statName={"neutral"} data={neutral} />
+                  <StatisticLine statName={"bad"} data={bad} />
+                  <StatisticLine statName={"all"} data={total} />
+                  <StatisticLine statName={"average"} data={average} />
+                  <StatisticLine statName={"positive"} data={positive} />
                 </tbody>
               </table>
               : <p> No feedback given </p>
