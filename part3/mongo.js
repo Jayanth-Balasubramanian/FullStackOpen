afc7dfa3ab00ee34981c61b48b65ea67606cbe59
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2022-06-25 22:57:15 +0530

    Part 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const password = process.argv[2]
+
+const url = `mongodb+srv://Jayanth:${password}@cluster0.hrtbsv5.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(url)
+    .then(result => {
+      console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+      console.log('error connecting to MongoDB:', error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv[3] && process.argv[4]) {
+  const personName = process.argv[3]
+  const personNum = process.argv[4]
+  const newPerson = new Person({
+  name: personName,
+  number: personNum
+  })
+  newPerson.save().then(() => {
+    console.log(`added ${personName} number ${personNum} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    console.log("Phonebook")
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
