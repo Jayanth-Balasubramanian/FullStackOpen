7715a81d73bd6e9b294ed9fcd9d58739683895e0
Jayanth Balasubramanian <jayanth.balasubramanian1@gmail.com>
2022-07-05 17:10:22 +0530

    Parts 4.1 - 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.MONGODB_URL,
+    {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+      logger.info('connected to MongoDB');
+    })
+    .catch((error) => {
+      logger.error('error connecting to MongoDB:', error.message);
+    });
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
